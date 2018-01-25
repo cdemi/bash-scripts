d30cd82b0e4aa24bbaa68105fa2552dcc8f7850b
Christopher Demicoli <christopher.demicoli@outlook.com>
2018-01-25 22:37:11 +0100

    Added Docker-Compose

--- ubuntu-server-setup.sh
+++ ubuntu-server-setup.sh
@@ -9,7 +9,12 @@ npm install --global speedtest-net
 
 #Install Docker
 curl -fsSL get.docker.com | bash
-apt-get install -y docker-compose
+#Install Docker-Compose
+curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+curl -L https://raw.githubusercontent.com/docker/compose/1.18.0/contrib/completion/bash/docker-compose -o /etc/bash_completion.d/docker-compose
+
+
 
 #Set Hostname
 read -p "What is the machine's full hostname? " hostname
