b533974170bffdd477e2a4a6c11644e5b176f0ca
Christopher Demicoli <cd17aap@herts.ac.uk>
2018-02-02 13:46:26 +0100

    Upgraded Script

--- softether-setup.sh
+++ softether-setup.sh
@@ -3,6 +3,8 @@ apt-get update
 apt-get install -y wget build-essential
 cd /tmp
 wget -qO- http://www.softether-download.com/files/softether/v4.25-9656-rtm-2018.01.15-tree/Linux/SoftEther_VPN_Server/64bit_-_Intel_x64_or_AMD64/softether-vpnserver-v4.25-9656-rtm-2018.01.15-linux-x64-64bit.tar.gz | tar xvz
-mv vpnserver/ /opt/
-cd /opt/vpnserver/
+mkdir /opt/softether
+mv vpnserver/* /opt/softether
+rm -rf vpnserver
+cd /opt/softether/
 make i_read_and_agree_the_license_agreement
